9902bc9c08efe2b6b29d8f7e5454744bb68d8776
laddu344 <valmiki.9398@gmail.com>
2025-10-08 00:37:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'Homepages')));
+app.use(express.static(path.join(__dirname, 'Landingpage')));
+app.use(express.static(path.join(__dirname, 'menspage')));
+app.use(express.static(path.join(__dirname, 'womensData')));
+app.use(express.static(path.join(__dirname, 'wishlist')));
+app.use(express.static(path.join(__dirname, 'payment')));
+app.use(express.static(path.join(__dirname, 'profile')));
+app.use(express.static(path.join(__dirname, 'Home Living Products')));
 
-// For root route
+// Serve homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Homepages', 'index.html'));
 });
 
-// Start the server
+// Other routes can be added similarly
+// Example:
+// app.get('/mens', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'menspage', 'index.html'));
+// });
+
 app.listen(port, () => {
   console.log(`Myntra Clone app listening at http://localhost:${port}`);
 });

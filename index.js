c400f279775cfe86f1e6f6c8a0857205ae9d37a2
laddu344 <valmiki.9398@gmail.com>
2025-10-08 00:30:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Simple route
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// For root route
 app.get('/', (req, res) => {
-  res.send('Hello from Myntra Node App!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Myntra Clone app listening at http://localhost:${port}`);
 });
